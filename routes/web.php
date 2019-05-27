30779ae9349edecc6004ea1fe51053def930e8f3
Putri <putrinapr@gmail.com>
2019-05-27 19:35:20 +0700

    new template

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@
 use App\Article;
 
 Route::get('/', function () {
-	$article = Article::all();
-    return view('welcome',compact('article'));
+    return view('welcome1');
+});
+
+Route::get('/b', function () {
+ $article = Article::all();
+   return view('welcome',compact('article'));
 })->name('welcome');
 
 Route::get('/articles', function () {
@@ -36,9 +40,9 @@ Route::prefix('admin')
         Route::resource('article','ArticleController');
         Route::get('/article/posts/{id}', 'ArticleController@posts')->name('article.posts');
         Route::prefix('article')->name('article.')->group(function () {
-            
+
         });
-        
+
         Route::resource('user','UserController');
     });
 
@@ -49,4 +53,3 @@ Route::prefix('admin')
 // // Route::resource('article','ArticleController');
 
 Auth::routes();
-
